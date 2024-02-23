c6fe4f512f688e4cbd341d38fcf96aa02c6fd815
valty-coder <124919535+valty-coder@users.noreply.github.com>
2024-02-23 21:03:38 +0300

    Add files via upload

--- Account.cs
+++ Account.cs
@@ -6,24 +6,40 @@ using System.Threading.Tasks;
 
 namespace casino_Work
 {
-    internal class Account : Balance
+    internal class Account 
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string Password { get; set; } 
+        public string Password { get; set; }
         public string? Email { get; set; }
         public string? PhoneNumber { get; set; }
-        public Balance balance { get; set; }
+        public int balance { get; set; }
 
-        public List<int> BalanceHistory;
+        public List<int> BalanceHistory = new List<int>();
+
+
+        public void ChangeBalance(int x)
+        {
+            balance = balance + x;
+        }
+        public void BalanceHistoryUpDate(int balance)
+        {
+            BalanceHistory.Add(balance);
+        }
+
+
 
         //List<Game> GameHistory { get; set; } заготовка для gameHistory
-        
+
 
         public void Print()
         {
             Console.WriteLine($"Account id: {Id}\nAccount Name: {Name}\nAccount balance: {balance} ");
         }
-
     }
+    
+
+    
+
 }
+
